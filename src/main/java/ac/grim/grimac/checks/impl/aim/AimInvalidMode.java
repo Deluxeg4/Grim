88170d4479c69fd3e5556ac990b9d1b44931982a
Hacker 2025 <Hacker 2025@gma>
2025-04-19 15:05:28 +0700

    fix AimInvalidMode

--- src/main/java/ac/grim/grimac/checks/impl/aim/AimInvalidMode.java
+++ src/main/java/ac/grim/grimac/checks/impl/aim/AimInvalidMode.java
@@ -10,49 +10,67 @@ import ac.grim.grimac.utils.math.GrimMath;
 
 @CheckData(name = "AimInvalidMode")
 public class AimInvalidMode extends Check implements RotationCheck {
+
+    private double lastModeX = 0;
+    private double lastModeY = 0;
+
+    private int xRotationsSinceModeMismatch = 0;
+    private int yRotationsSinceModeMismatch = 0;
+
+    private int maxRotationsBuffer;
+
     public AimInvalidMode(GrimPlayer playerData) {
         super(playerData);
     }
 
-    double lastModeX = 0;
-    double lastModeY = 0;
-    int xRotsSinceModeChange = 0, yRotsSinceModeChange = 0;
-    int maxRots;
-
-
     @Override
     public void process(final RotationUpdate rotationUpdate) {
+        double currentModeX = rotationUpdate.getProcessor().modeX;
+        double currentModeY = rotationUpdate.getProcessor().modeY;
 
-        double modeX = rotationUpdate.getProcessor().modeX;
-        double modeY = rotationUpdate.getProcessor().modeY;
+        boolean modeMismatch = (currentModeX == lastModeX) != (currentModeY == lastModeY);
+        boolean validLastModes = lastModeX != 0 && lastModeY != 0 && lastModeX < 1 && lastModeY < 1;
 
+        if (modeMismatch && validLastModes) {
+            boolean suspiciousX = isSuspiciousRotation(
+                    rotationUpdate.getDeltaXRotABS(),
+                    rotationUpdate.getProcessor().divisorX
+            );
+            boolean suspiciousY = isSuspiciousRotation(
+                    rotationUpdate.getDeltaYRotABS(),
+                    rotationUpdate.getProcessor().divisorY
+            );
 
-        if (((modeX == lastModeX) != (modeY == lastModeY)) && (lastModeX != 0 && lastModeY != 0 && lastModeX < 1 && lastModeY < 1)) {
-            if (rotationUpdate.getDeltaXRotABS() > 0 && rotationUpdate.getDeltaXRotABS() < 5 && rotationUpdate.getProcessor().divisorX > GrimMath.MINIMUM_DIVISOR) {
-                xRotsSinceModeChange++;
+            if (suspiciousX) xRotationsSinceModeMismatch++;
+            if (suspiciousY) yRotationsSinceModeMismatch++;
+
+            if (xRotationsSinceModeMismatch > maxRotationsBuffer && yRotationsSinceModeMismatch > maxRotationsBuffer) {
+                if (flagAndAlert("Detected invalid rotation mode mismatch (modeX=" + currentModeX +
+                        ", lastModeX=" + lastModeX + ", modeY=" + currentModeY + ", lastModeY=" + lastModeY + ")")) {
+                    // Optional: reset counts to avoid spamming
+                    xRotationsSinceModeMismatch = 0;
+                    yRotationsSinceModeMismatch = 0;
+                }
             }
-            if (rotationUpdate.getDeltaYRotABS() > 0 && rotationUpdate.getDeltaYRotABS() < 5 && rotationUpdate.getProcessor().divisorY > GrimMath.MINIMUM_DIVISOR) {
-                yRotsSinceModeChange++;
-            }
 
-            //Need some buffer bc the player can change sensitivity
-            if (xRotsSinceModeChange > maxRots && yRotsSinceModeChange > maxRots) {
-                flagAndAlert("modeX=" + modeX + " lmodeX=" + lastModeX + " modeY=" + modeY + " lmodeY=" + lastModeY);
-            }
-
-            return; //return here so the check keeps flagging till both modes change
-        } else {
-            xRotsSinceModeChange = yRotsSinceModeChange = 0;
+            // Do not reset counts here so it continues flagging
+            return;
         }
-        lastModeX = modeX;
-        lastModeY = modeY;
 
+        // Reset when modes sync again
+        xRotationsSinceModeMismatch = 0;
+        yRotationsSinceModeMismatch = 0;
 
+        lastModeX = currentModeX;
+        lastModeY = currentModeY;
+    }
+
+    private boolean isSuspiciousRotation(double delta, double divisor) {
+        return delta > 0 && delta < 5 && divisor > GrimMath.MINIMUM_DIVISOR;
     }
 
     @Override
     public void onReload(ConfigManager config) {
-        maxRots = config.getIntElse(getConfigName() + ".maxRots", 80);
-
+        maxRotationsBuffer = config.getIntElse(getConfigName() + ".maxRots", 80);
     }
 }
